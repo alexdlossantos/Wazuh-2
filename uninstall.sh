396d7a8e1d6216ef4ea27227aeaf72e0ee30446a
Kevin Gomez Gonzalez <kev.gomez97@hotmail.com>
2018-10-25 14:29:41 -0500

    Limpieza de git (Errores solucionados)

--- uninstall.sh
+++ uninstall.sh
@@ -3,3 +3,6 @@ docker rm -f elasticsearch_wazuh
 docker rm -f logstash_wazuh
 docker rm -f wazuh
 docker rm -f kibana_wazuh
+rm -rf /var/containers/wazuh
+
+echo "El servidor de Wazuh se ha desinstalado"
